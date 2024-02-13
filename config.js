7a57ef042ee6572ecc3c35e562d2693dc03460f9
DuTaCouple <159796621+DuTaCouple@users.noreply.github.com>
2024-02-13 14:14:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     introTitle: 'Duyên à! 😉',
-    introDesc: `Trái đất vốn lạ thường
+    introDesc: `           Trái đất vốn lạ thường     
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
